9025e43e6e29d20905644405f3c0b36bb1f974ca
Arlo <arlomltb@gmail.com>
2021-07-31 23:17:40 -0700

    Add default traversible function and map function

--- toolbox/components/grid.lua
+++ toolbox/components/grid.lua
@@ -9,11 +9,18 @@ Concord.component("grid", function(c, args)
   c.width = args.width
   c.height = args.height
   c.cell_size = args.cell_size
-  c.traversable = args.traversable or function(self) return true end
+  c.traversable = args.traversable or function(self, x, y) return true end
   c.data = {}
   -- initialize first dimension, so data can be accessed as 2d
   for x=1,c.width do
     c.data[x] = {}
+  end
+  function c:map(mapFunc)
+    for x=1,self.width do
+      for y=1,self.height do
+        mapFunc(c,x,y)
+      end
+    end
   end
   function c:get(x, y)
     if x < 1 or
